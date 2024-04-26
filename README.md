c14c56571dc2c976966129606e1db20a03ed869d
caseV12 <casevdev@gmail.com>
2024-04-26 17:30:47 +0900

    Chapter 5: Navigating Between Pages

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+
+### Chapter 5 | Navigating Between Pages
+
+Link는 a와 달리 전체 페이지 새로고침 없이 SPA처럼 동작할 수 있게함.
+Next는 route segment 별로 자동으로 code spliting을 함.
+React에서 페이지에 lazy 걸었던 것과 같음
+React는 code spliting 직접 안할 시 초기 로드시에 전체 코드 로딩함.
+
+---
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,20 +22,31 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
+        //### Chapter 5 | Navigating Between Pages
+        //Link는 a와 달리 전체 페이지 새로고침 없이 SPA처럼 동작할 수 있게함.
+        //Next는 route segment 별로 자동으로 code spliting을 함.
+        //React에서 페이지에 lazy 걸었던 것과 같음
+        //React는 code spliting 직접 안할 시 초기 로드시에 전체 코드 로딩함.
       })}
     </>
   );
